fb0230ec474f3eb64605784facbe4843c24dce11
Ilya Vasilyev <vasilyev.i123@gmail.com>
2023-08-22 11:37:15 +0300

    Add files via upload

--- scripts/set-theme.js
+++ scripts/set-theme.js
@@ -1,33 +1,42 @@
-const themeButtons = document.querySelectorAll('.theme-menu__button');
-
-themeButtons.forEach((button) => {
-  button.onclick = () => {
-    changeTheme(button.getAttribute('data-theme'));
-  };
-});
-
 function changeTheme(theme) {
-  document.body.className = 'page';
-  document.body.classList.add(`theme-${theme}`);
-  setDisabled(theme);
+  document.documentElement.className = '';
+  document.documentElement.classList.add(`theme-${theme}`);
   localStorage.setItem('theme', theme);
 }
 
-function initTheme() {
+(function initTheme() {
   const theme = localStorage.getItem('theme');
   if (theme) {
     changeTheme(theme);
   }
-}
+})();
 
-function setDisabled(theme) {
-  themeButtons.forEach((item) => {
-    if (item.getAttribute('data-theme') === theme) {
-      item.setAttribute('disabled', true);
-    } else {
-      item.removeAttribute('disabled');
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.documentElement;
+  const themeButtons = document.querySelectorAll('.theme-menu__button');
+
+  function setDisabled(theme) {
+    themeButtons.forEach((item) => {
+      if (item.getAttribute('data-theme') === theme) {
+        item.setAttribute('disabled', true);
+      } else {
+        item.removeAttribute('disabled');
+      }
+    });
+  }
+
+  if ([...root.classList].includes('theme-light')) {
+    setDisabled('light');
+  } else if ([...root.classList].includes('theme-dark')) {
+    setDisabled('dark');
+  } else {
+    setDisabled('auto');
+  }
+
+  themeButtons.forEach((button) => {
+    button.onclick = () => {
+      changeTheme(button.getAttribute('data-theme'));
+      setDisabled(button.getAttribute('data-theme'));
+    };
   });
-}
-
-initTheme();
+});
